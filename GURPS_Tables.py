90ad5324ef09e4f2bd1cf92db5e1d56de433327b
Jan Tschopp <tschopp_jan@gmx.net>
2014-07-10 14:02:32 +0200

    Add SequenceTable to the tables
    SequenceTable contains the wording for the different types of sequences
    on which the stars can be.

--- GURPS_Tables.py
+++ GURPS_Tables.py
@@ -55,7 +55,7 @@ StEvoTable = {
     'Gspan': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.1,
               1.0, 0.8, 0.7, 0.6, 0.6, 0.5, 0.4, 0.4, 0.4, 0.3, 0.3,
               0.2, 0.2, 0.2, 0.1, 0.1]
-    }
+}
 
 # The purpose of IndexTable is to facilitate lookups in StEvoTable.
 # The contents of IndexTable are the indices of the respective entries in
@@ -100,3 +100,11 @@ IndexTable = [
     # Indices 14-18
     [0] * 19, [0] * 19, [0] * 19, [0] * 19, [0] * 19
 ]
+
+# SequenceTable is used to look up the name of the sequence on which the star is
+SequenceTable = {
+    0: 'Main',
+    1: 'Subgiant',
+    2: 'Giant',
+    3: 'White Dwarf'
+}

c4c0cb0f32a64d50c57c2b5f12dfc6832bebc760
Jan Tschopp <tschopp_jan@gmx.net>
2014-07-10 14:17:43 +0200

    Add Star.findsequence()
    Add a class method to Star that finds the sequence the star is on. This
    will be used in future calculations, and is stored in the private
    variable __SeqIndex (which, incidentally, can then be used to get the
    wording out of SequenceTable).

--- GURPS_Star.py
+++ GURPS_Star.py
@@ -30,6 +30,7 @@ class Star:
 
     def setAge(self, age):
         self.__age = age
+        # Eventually some recalculations will have to be done
 
     def makeindex(self):
         # Roll to randomly select the index for the StEvoTable
@@ -39,3 +40,34 @@ class Star:
 
     def makemass(self):
         self.__mass = StEvoTable['mass'][self.__StEvoIndex]
+
+    def findsequence(self):
+        # Check what sequences are available
+        seq = StEvoTable['internaltype'][self.__StEvoIndex]
+        age = self.__age
+
+        # If we have a main-sequence-only star
+        if seq == 0:
+            self.__SeqIndex = 0
+
+        # If we have a main-sequence-only star that can decay to a white dwarf
+        elif seq == 1:
+            span = StEvoTable['Mspan'][self.__StEvoIndex]
+            if age > span:
+                self.__SeqIndex = 3
+            else:
+                self.__SeqIndex = 0
+
+        # If we have a star with sub- and giant type capabilities
+        elif seq == 2:
+            mspan = StEvoTable['Mspan'][self.__StEvoIndex]
+            sspan = StEvoTable['Sspan'][self.__StEvoIndex]
+            gspan = StEvoTable['Gspan'][self.__StEvoIndex]
+            if age > gspan:
+                self.__SeqIndex = 3
+            elif age > sspan:
+                self.__SeqIndex = 2
+            elif age > mspan:
+                self.__SeqIndex = 1
+            else:
+                self.__SeqIndex = 0

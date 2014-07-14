68c55e51750a7fc43c583671aafc4384c007d991
Jan Tschopp <tschopp_jan@gmx.net>
2014-07-14 20:48:59 +0200

    Add barebones GURPS_OrbitContents
    Add a skeleton file that outlines the contents of GURPS_OrbitContents.
    
    This file will then be fleshed out with the class methods. A good way
    to implement expanding of the __init__ method is needed.

---
+++ GURPS_OrbitContents.py
@@ -0,0 +1,12 @@
+# Here live all the GURPS Orbit Contents; Planets and Asteroid Belts
+
+class OrbitContent:
+
+
+class Planet(OrbitContent):
+
+class GasPlanet(Planet):
+
+class TerrPlanet(Planet):
+
+class AsteroidBelt(OrbitContent):

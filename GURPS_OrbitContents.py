c24fec6e932de12958f04da8bf3887bfe867a233
Jan Tschopp <tschopp_jan@gmx.net>
2014-07-15 01:45:32 +0200

    Add blackbody temperature to OrbitContent
    Add the capability of calculating the blackbody temperature for the
    OrbitContent class.
    
    Change the names of the classes somewhat and make GasGiant a child of
    OrbitContent instead of World.

--- GURPS_OrbitContents.py
+++ GURPS_OrbitContents.py
@@ -1,12 +1,29 @@
 # Here live all the GURPS Orbit Contents; Planets and Asteroid Belts
+import GURPS_Dice as GD
 
 class OrbitContent:
+    def roll(self, dicenum, modifier):
+        return self.roller.roll(dicenum, modifier)
 
+    def __init__(self,
+                 primarylum,    # Primary star's luminosity
+                 orbitalradius):
+        self.roller = GD.DiceRoller()
+        self.__orbit = orbitalradius
+        self.makebbtemp(primarylum, self.__orbit)
 
-class Planet(OrbitContent):
+    def makebbtemp(self, lum, orb):
+        self.__bbtemp = 278 * lum**(0.25) * orb**(-0.5)
 
-class GasPlanet(Planet):
+    def getBBTemp(self):
+        return self.__bbtemp
 
-class TerrPlanet(Planet):
+class World(OrbitContent):
+    pass
+#class TerrPlanet(Planet):
 
 class AsteroidBelt(OrbitContent):
+    pass
+
+class GasGiant(OrbitContent):
+    pass

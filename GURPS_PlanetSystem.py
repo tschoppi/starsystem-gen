483b69d2985c4ff7c50a04c700dfb934fb389fef
Jan Tschopp <tschopp_jan@gmx.net>
2014-07-18 18:17:18 +0200

    Complete placement of first gas giant
    Complete the placement of the first gas giant, according to the gas
    giant arrangement.
    
    Add a class method "printinfo" to PlanetSystem.

--- GURPS_PlanetSystem.py
+++ GURPS_PlanetSystem.py
@@ -15,7 +15,17 @@ class PlanetSystem:
             self.__innerforbidden = innerforbidden
             self.__outerforbidden = outerforbidden
             self.__forbidden = True
+        self.makegasgiantarrangement()
+        self.placegasgiant()
+        self.createorbits()
 
+    def printinfo(self):
+        print("--------------------")
+        print(" Planet System Info ")
+        print("--------------------")
+        print("GG Arrngmnt:\t{}".format(self.__gasarrangement))
+        print("Frst GG Orb:\t{}".format(self.__firstgasorbit))
+        print("     Orbits:\t{}".format(self.__orbitarray))
 
     def allowedorbit(self, testorbit):
         result  = testorbit >= self.__innerlimit
@@ -26,3 +36,27 @@ class PlanetSystem:
             return result & result2
         else:
             return result
+
+    def makegasgiantarrangement(self):
+        dice = self.roll(3,0)
+        self.__gasarrangement = 'None'
+        if dice > 10:
+            self.__gasarrangement = 'Conventional'
+        if dice > 12:
+            self.__gasarrangement = 'Eccentric'
+        if dice > 14:
+            self.__gasarrangement = 'Epistellar'
+
+    def placegasgiant(self):
+        orbit = 0
+        if self.__gasarrangement == 'Conventional':
+            orbit = (1 + (self.roll(2,-2) * 0.05)) * self.__snowline
+        if self.__gasarrangement == 'Eccentric':
+            orbit = self.roll(1,0) * 0.125 * self.__snowline
+        if self.__gasarrangement == 'Epistellar':
+            orbit = self.roll(3,0) * 0.1 * self.__innerlimit
+        self.__firstgasorbit = orbit
+
+
+    def createorbits(self):
+        self.__orbitarray = []

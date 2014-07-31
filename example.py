f246d244903c7c8d2247b80ee7145fc7c936175f
Jan Tschopp <tschopp_jan@gmx.net>
2014-07-31 12:57:38 +0200

    Add example python script
    Add an example script that generates a StarSystem.

---
+++ example.py
@@ -0,0 +1,12 @@
+"""This is an example script for automated star system generation"""
+
+import gurps.space.starsystem as starsys
+
+# Change from None to a value if you want to set an argument
+args = {
+    'opencluster': None, # True or False
+    'numstars': None, # 1, 2 or 3
+    'age': None # Number > 0
+}
+
+mysys = starsys.StarSystem(**args)

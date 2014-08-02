83685cd3cb0b5c934b5abfab4493fec681d77dd2
Jan Tschopp <tschopp_jan@gmx.net>
2014-08-02 12:35:04 +0200

    Add Marginal Atmosphere Roll Table
    Add a table to look up marginal atmospheres from a 3d roll (GURPS Space
    p. 81).

--- gurps/space/tables.py
+++ gurps/space/tables.py
@@ -147,3 +147,13 @@ SizeToInt = {
 IntToSize = [
     "Tiny", "Small", "Standard", "Large"
 ]
+
+# Marginal Atmosphere Table
+# Usage: MAtmoTable[diceroll], diceroll is between 3 and 18
+MAtmoTable = [
+    None, None, None, 'Chlorine', 'Chlorine', 'Sulfur Compounds',
+    'Sulfur Compounds', 'Nitrogen Compounds', 'Organic Toxins',
+    'Organic Toxins', 'Low Oxygen', 'Low Oxygen',
+    'Pollutants', 'Pollutants', 'High Carbon Dioxide',
+    'High Oxygen', 'High Oxygen', 'Inert Gases', 'Inert Gases'
+]

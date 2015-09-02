30f63278b122602538d65cb58348aa238c991e05
myrion <myrion_the_first@hotmail.com>
2015-09-02 11:33:34 +0200

    Initial commit for a cherrypy-based GUI.

---
+++ web-gui/server.py
@@ -0,0 +1,30 @@
+import cherrypy
+import mimetypes
+
+## mimetypes.types_map['.csv'] = 'text/csv'
+## This sets a mimetype manually, in case mimetypes doesn't guess right
+
+
+## [/style.css]
+## tools.staticfile.on = True
+## tools.staticfile.filename = "/home/site/style.css"
+## This serves a  single static file MUST BE ABSOLUTE PATH!
+
+## [/static]
+## tools.staticdir.on = True
+## tools.staticdir.dir = "/home/site/static"
+## tools.staticdir.index = "index.html"
+## This serves a directory MUST BE ABSOLUTE PATH! Windows?
+
+ 
+class WebServer(object):
+    @cherrypy.expose
+    def hello(self):
+        return "<h1>Hello world!</h1>"
+
+if __name__ == '__main__':
+   ## This line reads the global server config from the file
+   cherrypy.config.update("server.conf")
+   ## The third argument reads the application config from the file.
+   ## This is necessary, because apparently the route mapping is application-specific.
+   cherrypy.quickstart(WebServer(), '/', "server.conf")

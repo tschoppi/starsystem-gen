6a67d4666e025fcfec6da102bf2c4cab40d3dca2
Mark Zeman <myrion211@gmail.com>
2016-01-12 14:38:58 +0100

    Solar system now has super jerky animation.

--- webgui/scripts/planets.js
+++ webgui/scripts/planets.js
@@ -1,5 +1,77 @@
+function animate_solar_system(toDraw, star_letter, sweet_spot_scale, context, canvas, centerX, centerY, body_size) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (getCookie("positionOnEllipse") == ""){
+        var position_on_ellipse = 1;
+    } else {
+        var position_on_ellipse = parseInt(getCookie("positionOnEllipse"));
+    }
+    for (var i = 0; i < toDraw.length; i++) {
+        if (getCookie(i + star_letter) == "") {
+            // We store the rotation belonging to a given astro_body in a cookie
+            document.cookie = i + star_letter + "=" + Math.floor((Math.random() * 360) + 1);
+        }
+        var cookieValue = parseInt(getCookie(i + star_letter), 10);
+        var semi_major = toDraw[i].max * sweet_spot_scale;
+        var semi_minor = toDraw[i].min * sweet_spot_scale;
+
+        // Draw the ellipse
+        context.beginPath();
+        context.ellipse(centerX, centerY, semi_major, semi_minor, cookieValue, 0, Math.PI * 2);
+        context.stroke();
+        context.closePath();
+
+        // Write the label; offset 10 and 15 pixels respectively from the ellipse
+        var text_x = centerX + (semi_major * Math.cos(position_on_ellipse) * Math.cos(cookieValue) - semi_minor * Math.sin(position_on_ellipse) * Math.sin(cookieValue));
+        var text_y = centerY + (semi_major * Math.cos(position_on_ellipse) * Math.sin(cookieValue) + semi_minor * Math.sin(position_on_ellipse) * Math.cos(cookieValue));
+        var left_right = text_x > centerX ? 1 : -2;
+        var up_down = text_y > centerY ? 1 : -1.5;
+        context.fillStyle = 'white';
+        var rectWidth = context.measureText(star_letter + "-" + (i + 1)).width;
+        context.fillRect((left_right * 10) + text_x, (up_down * 12) + text_y, rectWidth + 10, 20);
+        context.beginPath();
+        context.fillStyle = 'black';
+        context.textBaseline = 'top';
+        context.fillText(star_letter + "-" + (i + 1), (left_right * 10) + text_x, (up_down * 12) + text_y);
+
+        // Draw a dot to represent the astronomical body
+        context.beginPath();
+        context.arc(text_x, text_y, 3, 0, 2 * Math.PI, false);
+        context.fill();
+        context.stroke();
+
+        // Draw the star last, so it will always be visible
+        context.beginPath();
+        context.arc(centerX, centerY, body_size, 0, 2 * Math.PI, false);
+        context.fillStyle = 'yellow';
+        context.fill();
+        context.stroke();
+        context.fillStyle = 'black';
+
+        if (position_on_ellipse < 6){
+            position_on_ellipse += 1;
+            console.log(position_on_ellipse);
+        } else {
+            position_on_ellipse = 0;
+        }
+        document.cookie = "positionOnEllipse=" + position_on_ellipse;
+    }
+}
+
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var cookieArray = document.cookie.split(';');
+    for(var i=0; i<cookieArray.length; i++) {
+        var c = cookieArray[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+
 document.onreadystatechange = function(){
     if (document.readyState === 'complete'){
+        document.cookie = "positionOnEllipse=";
         var canvas = document.getElementById("diagram");
         var star_size = parseFloat(document.getElementById("star-radius").innerHTML);
         var outer_limit = parseFloat(document.getElementById("star-limit").innerHTML);
@@ -13,14 +85,14 @@ document.onreadystatechange = function(){
         var rows = overviewTable.getElementsByTagName("tr");
         var toDraw = [];
         var max_size = 0;
-        for (row in rows){
+        for (var row in rows){
             // Row 0 contains the headings, the others are javascript attributes of the object
             if (row !== '0' && row !== 'item' && row !== 'length' && row !== 'namedItem'){
                 var min_cell = document.getElementById('min_radius' + row);
                 var max_cell = document.getElementById('max_radius' + row);
 
-                var min_radius = parseFloat(min_cell.innerHTML, '10');
-                var max_radius = parseFloat(max_cell.innerHTML, '10');
+                var min_radius = parseFloat(min_cell.innerHTML).toFixed(1   );
+                var max_radius = parseFloat(max_cell.innerHTML).toFixed(1);
 
                 // scale the radii
                 min_radius = (min_radius * diagram_scale);
@@ -64,62 +136,9 @@ document.onreadystatechange = function(){
                     "<br/> The star is to scale."
         }
 
-
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < toDraw.length; i++){
-            if (getCookie(i + star_letter) == ""){
-                // We store the rotation belonging to a given astro_body in a cookie
-                document.cookie = i + star_letter + "=" + Math.floor((Math.random() * 360) + 1);
-            }
-            cookieValue = parseInt(getCookie(i + star_letter), '10');
-            var semi_major = toDraw[i].max * sweet_spot_scale;
-            var semi_minor = toDraw[i].min * sweet_spot_scale;
-
-            // Draw the ellipse
-            context.beginPath();
-            context.ellipse(centerX, centerY, semi_major, semi_minor, cookieValue, 0, Math.PI*2);
-            context.stroke();
-            context.closePath();
-
-            // Write the label; offset 10 and 15 pixels respectively from the ellipse
-            var text_x = centerX + (semi_major * Math.cos(1) * Math.cos(cookieValue) - semi_minor * Math.sin(1) * Math.sin(cookieValue));
-            var text_y = centerY + (semi_major * Math.cos(1) * Math.sin(cookieValue) + semi_minor * Math.sin(1) * Math.cos(cookieValue));
-            var left_right = text_x > centerX ? 1 : -2;
-            var up_down = text_y > centerY ? 1 : -1.5;
-            context.fillStyle = 'white';
-            var rectWidth = context.measureText(star_letter + "-" + (i + 1)).width;
-            context.fillRect((left_right * 10) + text_x, (up_down * 12) + text_y, rectWidth + 10, 20);
-            context.beginPath();
-            context.fillStyle = 'black';
-            context.textBaseline = 'top';
-            context.fillText(star_letter + "-" + (i + 1), (left_right * 10) + text_x, (up_down * 12) + text_y);
-
-            // Draw a dot to represent the astronomical body
-            context.beginPath();
-            context.arc(text_x, text_y, 3, 0, 2 * Math.PI, false);
-            context.fill();
-            context.stroke();
-        }
-
-        // Draw the star last, so it will always be visible
-        context.beginPath();
-        context.arc(centerX, centerY, body_size, 0, 2 * Math.PI, false);
-        context.fillStyle = 'yellow';
-        context.fill();
-        context.stroke();
-        context.fillStyle = 'black';
+        animate_solar_system(toDraw, star_letter, sweet_spot_scale, context, canvas, centerX, centerY);
+        window.setInterval(animate_solar_system, 1000, toDraw, star_letter, sweet_spot_scale, context, canvas, centerX, centerY, body_size);
     }
-}
-
-function getCookie(cookieName) {
-    var name = cookieName + "=";
-    var cookieArray = document.cookie.split(';');
-    for(var i=0; i<cookieArray.length; i++) {
-        var c = cookieArray[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
-    }
-    return "";
-}
+};
 
 // beginPath starts a new Path that can then be drawn with stroke

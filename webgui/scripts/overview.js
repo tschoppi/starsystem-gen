b354b487e1fb8f55d7724691fa208a5d131fc116
Mark Zeman <myrion211@gmail.com>
2016-11-01 11:12:15 +0100

    Made the "change settings" link server-independent

--- webgui/scripts/overview.js
+++ webgui/scripts/overview.js
@@ -1,5 +1,12 @@
 function regenerate(){
-    current_path = window.location.href;
+    current_path = window.location.href
     current_path = current_path.split("&seed")[0]
+    window.location.href = current_path
+}
+
+function go_back(){
+    current_path = window.location.href.split("/")
+    current_path.pop()
+    current_path = current_path.join("/")
     window.location.href = current_path
 }

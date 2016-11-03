573583278f743f39f2635a76cf82cf34184bc013
Mark Zeman <myrion211@gmail.com>
2016-11-03 09:16:41 +0100

    Made the "change settings" link server-independent

--- webgui/scripts/overview.js
+++ webgui/scripts/overview.js
@@ -8,5 +8,6 @@ function go_back(){
     current_path = window.location.href.split("/")
     current_path.pop()
     current_path = current_path.join("/")
+    current_path += "/"
     window.location.href = current_path
 }
